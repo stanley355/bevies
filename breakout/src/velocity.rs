b1630cfdd855b823dbb5d73435b5c4451bd91e76
Stanley Winata <winatastanley355@gmail.com>
2022-09-19 10:01:49 +0700

    something not working

--- breakout/src/velocity.rs
+++ breakout/src/velocity.rs
@@ -1,4 +1,14 @@
+use crate::breakout_plugin::TIME_STEP;
 use bevy::prelude::*;
-
 #[derive(Component, Deref, DerefMut)]
 pub struct Velocity(pub Vec2);
+
+// TODO: Not working here!
+// impl Velocity {
+//     pub fn apply_velocity(mut query: Query<&mut Transform, &Velocity>) {
+//         for (mut transform, velocity) in &mut query {
+//             // transform.translation.x += velocity.x * TIME_STEP;
+//             // transform.translation.y += velocity.y * TIME_STEP;
+//         }
+//     }
+// }
