56b538b11316c0b5105de3ce1d39f2a4c73bbd0c
Stanley Winata <winatastanley355@gmail.com>
2022-09-20 15:53:51 +0700

    Finally title is shown

--- pokemon/src/frame/plugin.rs
+++ pokemon/src/frame/plugin.rs
@@ -1,17 +1,18 @@
-use bevy::prelude::*;
 use super::title::Title;
+use bevy::prelude::*;
 
 #[derive(Debug)]
 pub struct FramePlugin;
 
 impl FramePlugin {
-    pub fn setup(mut commands: Commands) {
-        commands.spawn_bundle(Title::text_bundle());
+    pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+        let title_bundle = Title::text_bundle(asset_server);
+        commands.spawn_bundle(title_bundle);
     }
 }
 
 impl Plugin for FramePlugin {
-    fn build(&self, app: &mut App) {
+    fn build(&self, app: &mut App, ) {
         app.insert_resource(Title)
             .add_startup_system(FramePlugin::setup);
     }
--- pokemon/src/frame/title.rs
+++ pokemon/src/frame/title.rs
@@ -2,23 +2,24 @@ use bevy::prelude::*;
 
 const GAME_TITLE: &str = "Rust Pokemon";
 const TITLE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
-const TITLE_PADDING: Val = Val::Px(5.);
+const TITLE_PADDING: Val = Val::Px(10.);
 
 #[derive(Debug)]
 pub struct Title;
 
 impl Title {
-    pub fn text_bundle() -> TextBundle {
-        let style = Title::text_style();
+    pub fn text_bundle(asset_server: Res<AssetServer>) -> TextBundle {
+        let style = Title::text_style(asset_server);
         let position = Title::text_position();
         TextBundle::from_section(GAME_TITLE, style).with_style(position)
     }
 
-    pub fn text_style() -> TextStyle {
+    // TextStyle.font is required to make text visible
+    pub fn text_style(asset_server: Res<AssetServer>) -> TextStyle {
         TextStyle {
-            font: asset_server.load("fonts/FiraMono-Medium.ttf"),
             color: TITLE_COLOR,
-            ..default()
+            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+            font_size: 24.
         }
     }
 
