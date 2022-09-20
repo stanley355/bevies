e9d43a1036cbc22ffd6792c77d28525ceaebd0b4
Stanley Winata <winatastanley355@gmail.com>
2022-09-20 09:43:08 +0700

    Add camera and window initial setup

--- pokemon/src/main.rs
+++ pokemon/src/main.rs
@@ -1,3 +1,13 @@
+use bevy::{prelude::*, window};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(camera_setup)
+        .add_system(window::close_on_esc)
+        .run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
 }
