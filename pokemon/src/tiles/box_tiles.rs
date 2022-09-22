95f42b24d8bf79832fb1eb7a3ace9f7d1c98651a
Stanley Winata <winatastanley355@gmail.com>
2022-09-22 17:27:16 +0700

    Recognizes box tiles height and width

--- pokemon/src/tiles/box_tiles.rs
+++ pokemon/src/tiles/box_tiles.rs
@@ -1,5 +1,8 @@
 use bevy::{prelude::*, sprite::Rect};
 
+const BOX_TILES_WIDTH: f32 = 160.;
+const BOX_TILES_HEIGHT: f32 = 115.;
+
 #[derive(Debug)]
 pub enum BoxTiles {
     Default,
@@ -28,12 +31,11 @@ impl BoxTiles {
         texture_atlas_res: &mut ResMut<Assets<TextureAtlas>>,
     ) -> Handle<TextureAtlas> {
         let texture_asset = asset_server.load("sprites/box-sprites.png");
-        let mut texture_atlas =
-            TextureAtlas::from_grid(texture_asset, Vec2::new(200.0, 400.0), 1, 1);
-        // texture_atlas.textures = vec![Rect {
-        //     min: Vec2::new(400.0, 0.0),
-        //     max: Vec2::new(200.0, 400.0),
-        // }];
+        let mut texture_atlas = TextureAtlas::from_grid(texture_asset, Vec2::new(0.0, 0.0), 1, 1);
+        texture_atlas.textures = vec![Rect {
+            min: Vec2::new(0.0, (600.0 + 145. * 4.)),
+            max: Vec2::new(160.0, (600.0 + 145. * 4.) + 145.),
+        }];
 
         let texture_atlas_handle = texture_atlas_res.add(texture_atlas);
         return texture_atlas_handle;
