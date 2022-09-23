2c855fcb915fd3f4da7ebd0cb7c8199bf91d6bfc
Stanley Winata <winatastanley355@gmail.com>
2022-09-23 17:02:40 +0700

    Player transform

--- pokemon/src/player/player.rs
+++ pokemon/src/player/player.rs
@@ -4,30 +4,23 @@ use bevy::prelude::*;
 pub struct Player;
 
 impl Player {
-    // pub fn move_player(
-    //     keyboard_input: Res<Input<KeyCode>>,
-    //     mut query: Query<&mut Transform, With<Paddle>>,
-    // ) {
-    //     let mut paddle_transform = query.single_mut();
-    //     let mut direction = 0.0;
+    pub fn move_player(
+        keyboard_input: Res<Input<KeyCode>>,
+        mut query: Query<&mut Transform, With<Player>>,
+    ) {
+        for mut player_transform in query.iter_mut() {
+            let mut direction = 0.0;
 
-    //     if keyboard_input.pressed(KeyCode::Left) {
-    //         direction -= 1.0;
-    //     }
+            println!("{:?}", player_transform);
+            if keyboard_input.pressed(KeyCode::Left) {
+                println!("hihihihi");
+                player_transform.translation.x = 100.;
+            }
 
-    //     if keyboard_input.pressed(KeyCode::Right) {
-    //         direction += 1.0;
-    //     }
+            if keyboard_input.pressed(KeyCode::Right) {
+              player_transform.translation.x = -100.;
+            }
 
-    //     // Calculate the new horizontal paddle position based on player input
-    //     let new_paddle_position =
-    //         paddle_transform.translation.x + direction * PADDLE_SPEED * TIME_STEP;
-
-    //     // Update the paddle position,
-    //     // making sure it doesn't cause the paddle to leave the arena
-    //     let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 + PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
-    //     let right_bound = RIGHT_WALL - WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 - PADDLE_PADDING;
-
-    //     paddle_transform.translation.x = new_paddle_position.clamp(left_bound, right_bound);
-    // }
+        }
+    }
 }
--- pokemon/src/player/plugin.rs
+++ pokemon/src/player/plugin.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+use super::player::Player;
 use super::setup::PlayerSetup;
 
 #[derive(Debug)]
@@ -7,6 +8,7 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(PlayerSetup::sprite_setup);
+        app.add_system(PlayerSetup::sprite_setup)
+            .add_system_set(SystemSet::new().with_system(Player::move_player));
     }
 }
