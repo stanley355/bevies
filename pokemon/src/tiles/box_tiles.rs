de28455fc0ac63996fd531a5f39f0f8ad7ca971b
Stanley Winata <winatastanley355@gmail.com>
2022-09-23 09:42:41 +0700

    Box tiles struct done

--- pokemon/src/tiles/box_tiles.rs
+++ pokemon/src/tiles/box_tiles.rs
@@ -1,14 +1,49 @@
 use bevy::{prelude::*, sprite::Rect};
 
+use super::plugin::DEFAULT_VEC2;
+
 const BOX_TILES_WIDTH: f32 = 160.;
 const BOX_TILES_HEIGHT: f32 = 115.;
 
 const DEFAULT_BOX_SIZE: Vec2 = Vec2::new(BOX_TILES_WIDTH, BOX_TILES_HEIGHT);
 const PLYWOOD_BOX_POSITION: Vec2 = Vec2::new(484.0, 1122.);
 
-#[derive(Debug)]
-pub enum BoxTiles {
+#[derive(Debug, Copy, Clone)]
+pub enum BoxTilesType {
     Plywood,
+}
+
+impl BoxTilesType {
+    pub fn get_texture(self) -> Vec<Rect> {
+        let rect = self.get_rect();
+
+        vec![rect]
+    }
+
+    pub fn get_rect(self) -> Rect {
+        let mut rect = Rect {
+            min: Vec2::new(0., 0.),
+            max: DEFAULT_BOX_SIZE,
+        };
+
+        match self {
+            BoxTilesType::Plywood => {
+                rect.min = PLYWOOD_BOX_POSITION;
+                rect.max = Vec2::new(
+                    PLYWOOD_BOX_POSITION.x + DEFAULT_BOX_SIZE.x,
+                    PLYWOOD_BOX_POSITION.y + DEFAULT_BOX_SIZE.y,
+                );
+            }
+        }
+
+        rect
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct BoxTiles {
+    pub box_type: BoxTilesType,
+    pub transform: Transform,
 }
 
 impl BoxTiles {
@@ -18,11 +53,10 @@ impl BoxTiles {
         texture_atlas_res: &mut ResMut<Assets<TextureAtlas>>,
     ) -> SpriteSheetBundle {
         let texture_handle = self.texture_atlas_handler(asset_server, texture_atlas_res);
-        let transform = BoxTiles::set_transform();
 
         let sprite_sheet = SpriteSheetBundle {
             texture_atlas: texture_handle,
-            transform: transform,
+            transform: self.transform,
             ..default()
         };
 
@@ -35,37 +69,10 @@ impl BoxTiles {
         texture_atlas_res: &mut ResMut<Assets<TextureAtlas>>,
     ) -> Handle<TextureAtlas> {
         let texture_asset = asset_server.load("sprites/box-sprites.png");
-        let mut texture_atlas = TextureAtlas::from_grid(texture_asset, Vec2::new(1000., 0.), 1, 1);
-        texture_atlas.textures = self.set_box_rect();
+        let mut texture_atlas = TextureAtlas::from_grid(texture_asset, DEFAULT_VEC2, 1, 1);
+        texture_atlas.textures = self.box_type.get_texture();
 
         let texture_atlas_handle = texture_atlas_res.add(texture_atlas);
         return texture_atlas_handle;
-    }
-
-    pub fn set_transform() -> Transform {
-        Transform {
-            translation: Vec3::new(0., 0., 0.),
-            scale: Vec3::new(1., 1., 0.0),
-            ..default()
-        }
-    }
-
-    pub fn set_box_rect(self) -> Vec<Rect> {
-        let mut rect = Rect {
-            min: Vec2::new(0., 0.),
-            max: DEFAULT_BOX_SIZE,
-        };
-
-        match self {
-            BoxTiles::Plywood => {
-                rect.min = PLYWOOD_BOX_POSITION;
-                rect.max = Vec2::new(
-                    PLYWOOD_BOX_POSITION.x + DEFAULT_BOX_SIZE.x,
-                    PLYWOOD_BOX_POSITION.y + DEFAULT_BOX_SIZE.y,
-                );
-            }
-        }
-
-        vec![rect]
     }
 }
--- pokemon/src/tiles/plugin.rs
+++ pokemon/src/tiles/plugin.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
-
 use crate::tiles::box_tiles::BoxTiles;
+use super::box_tiles::BoxTilesType;
+
+pub const DEFAULT_VEC2: Vec2 = Vec2::new(0., 0.);
 
 #[derive(Debug)]
 pub struct TilesPlugin;
@@ -11,7 +13,15 @@ impl TilesPlugin {
         asset_server: Res<AssetServer>,
         mut texture_atlas_res: ResMut<Assets<TextureAtlas>>,
     ) {
-        let boxa = BoxTiles::new(BoxTiles::Plywood, &asset_server, &mut texture_atlas_res);
+        let new_box = BoxTiles {
+            box_type: BoxTilesType::Plywood,
+            transform: Transform {
+                translation: Vec3::new(0., 0., 0.),
+                scale: Vec3::new(1., 1., 0.0),
+                ..default()
+            },
+        };
+        let boxa = new_box.new(&asset_server, &mut texture_atlas_res);
 
         commands.spawn_bundle(boxa);
     }
