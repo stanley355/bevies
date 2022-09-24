9ad1d44b41e1891b64d0599a3618c3f0ae325060
Stanley Winata <winatastanley355@gmail.com>
2022-09-24 12:50:50 +0700

    remove lint cos of nightly

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
+
 mod window;
 
 pub const WINDOW_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 
 fn main() {
-    App::new()
-        .insert_resource(ClearColor(WINDOW_COLOR))
+    let mut app = App::new();
+    app.insert_resource(ClearColor(WINDOW_COLOR))
         .insert_resource(window::window_descriptor())
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
